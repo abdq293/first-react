54e520b38f17e6143cb79874f6bd6e5ab1ca5376
abdq293 <abd.kassar10@outlook.com>
2023-04-06 02:27:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />=
-        <h2>first react app</h2>
+        <img src={logo} className="App-logo" alt="logo" />=<h2>trying git</h2>
       </header>
     </div>
   );
